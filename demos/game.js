03306675868a85a0c2a87e887f1220d755bea3b1
Richard Key <rich@busyrich.com>
2011-10-17 21:28:13 -0700

    I have not committed in a while, so this is a big update. First off I moved all the essential game logic to a few objects the main of which is the engine object. It controls all the game functions such as the loop and holds the scene manager. I also created a scene manager which maintains the live canvases for each scene, and holds the base scene collection used in the game. Finally I added in the click event to get the events working as they were before I started this update. Now the game.js should only contain game-specific logic.

--- demos/game.js
+++ demos/game.js
@@ -1,3 +1,5 @@
+PFPlay.gameWindow = document.getElementById('gameWindow');
+
 var mario = new PFPlay.Sprite('img/mario.png', 'mario');
 var luigi = new PFPlay.Sprite('img/luigi.png', 'luigi');
 var cat = new PFPlay.Sprite('img/cat.jpg', 'cat');
@@ -16,42 +18,13 @@ var mAni = new PFPlay.Animation(
   "ma1", {x:125, y:125}, 4, 50
 );
 
-window.addEventListener('click', windowClick, false);
-
 var cybertron =  new PFPlay.Scene('Cybertron');
 
-function gameGo()
-{
-  mario.bind('click', function() { alert('clicked on mario!'); });
-  mario.bind('click', function() { alert('clicked on mario, round 2!'); });
-  mario.addAnimation(mAni);
-  mario.getAnimation('ma1').start();
-  mario.move(0, 200);
-  mario.zindex = 2;
-  
-  luigi.addAnimation(
-    'la1', {x:125, y:125}, 4, 100,
-    {'offset': {x:0, y:2}}
-  );
-  luigi.getAnimation('la1').start();
-  luigi.move(300, 0);
-  
-  world.addObject(mario);
-  world.addObject(luigi);
-  world.addObject(cat);
-  
-  cybertron.addLayer(world);
-  cybertron.addLayer(bg);
-  
-  document.getElementById('gameWindow').appendChild(cybertron.getScene());
-  
-  PFPlay.tick = 50;
-  setInterval("loop();", PFPlay.tick);
-}
+var myEngine = new PFPlay.Engine();
 
-function loop()
+function loop(sceneManager)
 {
-  var wLayer = cybertron.getLayer('myLayer');
+  var wLayer = sceneManager.getScene('Cybertron').getLayer('myLayer');
   
   wLayer.getObject('mario').move(10, 0);
   wLayer.getObject('luigi').move(0, 10);
@@ -72,34 +45,38 @@ function loop()
     wLayer.getObject('cat').visible = true;
   else
     wLayer.getObject('cat').visible = false;
- 
-  cybertron.update();
-  
-  PFPlay.masterTime = new Date().getTime() - start;
   
   var debugTime = document.getElementById('time');
   debugTime.innerText = PFPlay.masterTime;
 }
 
-function windowClick(evt)
+function gameGo()
 {
-  var offX = scene.offsetLeft;
-  var offY = scene.offsetTop;
+  mario.bind('click', function() { alert('clicked on mario!'); });
+  mario.addAnimation(mAni);
+  mario.getAnimation('ma1').start();
+  mario.move(0, 200);
+  mario.zindex = 2;
   
-  if(scene.offsetParent)
-  {
-    var parent = scene.offsetParent;
-    do {
-        offX += parent.offsetLeft;
-        offY += parent.offsetTop;
-    } while (parent = parent.offsetParent);
-  }
+  luigi.bind('click', function() { alert('clicked on luigi!'); });
+  luigi.addAnimation(
+    'la1', {x:125, y:125}, 4, 100,
+    {'offset': {x:0, y:2}}
+  );
+  luigi.getAnimation('la1').start();
+  luigi.move(300, 0);
   
-  var x = evt.clientX - offX;
-  var y = evt.clientY - offY;
+  cat.bind('click', function() { alert('clicked on cat!'); });
   
-  if(x >= 0 && x < scene.width && y >= 0 && y < scene.height)
-    world.raiseEvent('click', {x: x, y: y});
-  else
-    alert('im over here!');
+  world.addObject(mario);
+  world.addObject(luigi);
+  world.addObject(cat);
+  
+  cybertron.addLayer(world);
+  cybertron.addLayer(bg);
+  
+  myEngine.scenes.add(cybertron);
+  myEngine.scenes.activate('Cybertron');
+  
+  myEngine.go(50, loop);
 }
