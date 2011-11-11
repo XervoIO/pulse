bdc48fee069d3f2c7e2066be1c4367e4f551f3d1
Brandon Cannaday <brandon@paranoidferret.com>
2011-11-11 12:28:01 -0500

    Added pong demo.  Minor tweaks to engine.

---
+++ demos/pong/game.js
@@ -0,0 +1,50 @@
+function initGame() {
+  var engine = new PFPlay.Engine({ gameWindow: 'gameWindow', width: 760, height: 480 });
+  var scene = new PFPlay.Scene(); 
+  var layer = new PFPlay.Layer();
+  
+  var score = new Score( { text: "0 | 0" } );
+  score.position.x = 300;
+  score.position.y = 200;
+  
+  for(var i = 0; i < 5000; i++) {
+    var ball = new Ball({ src: 'ball.png', size: {width: 16, height: 16} });
+    ball.position.x = 300;
+    ball.position.y = 200;
+    ball.velocity.x = (Math.random() * .1) - 0.05;
+    ball.velocity.y = (Math.random() * .1) - 0.05;
+    ball.score = score;
+    layer.addNode(ball);
+  }
+  
+  var paddle = new Paddle({ src: 'paddle.png' });
+  paddle.ball = ball;
+  paddle.position.x = 20;
+  paddle.position.y = 150;
+  
+  var aiPaddle = new Paddle( { src: 'paddle.png' });
+  aiPaddle.ball = ball;
+  aiPaddle.position.x = 730;
+  aiPaddle.position.y = 150;
+  aiPaddle.auto = true;
+  
+  //layer.addNode(paddle);
+  //layer.addNode(aiPaddle);
+  layer.addNode(score);
+  
+  layer.anchor.x = 0;
+  layer.anchor.y = 0;
+  
+  scene.addLayer(layer);
+  
+  engine.scenes.addScene(scene);
+  engine.scenes.activateScene(scene);
+  
+  function loop(sceneManager)
+  { 
+    var debugTime = document.getElementById('time');
+    debugTime.innerText = debugTime.textContent = engine.masterTime;
+  }
+  
+  engine.go(20, loop);
+}
