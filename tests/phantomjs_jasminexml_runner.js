c411b9845975c76a06d5b22ce0d99d760e7376d3
Charlie Key <ckey@paranoidferret.com>
2012-01-27 15:08:15 -0500

    Fix for phantoms jasmine runner

--- tests/phantomjs_jasminexml_runner.js
+++ tests/phantomjs_jasminexml_runner.js
@@ -10,7 +10,7 @@ if ( phantom.args.length !== 2 ) {
     var htmlrunner = phantom.args[0],
         resultdir = phantom.args[1],
         page = new WebPage();
-    
+
     // Echo the output of the tests to the Standard Output
     page.onConsoleMessage = function(msg, source, linenumber) {
         console.log(msg);
@@ -27,20 +27,22 @@ if ( phantom.args.length !== 2 ) {
                 var suitesResults = page.evaluate(function(){
                     return jasmine.phantomjsXMLReporterResults;
                 });
-                
+
                 // Save the result of the tests in files
                 var f = null;
-                for ( var i = 0, len = suitesResults.length; i < len; ++i ) {
-                    try {
-                        f = fs.open(resultdir + '/' + suitesResults[i]["xmlfilename"], "w");
-                        f.write(suitesResults[i]["xmlbody"]);
-                        f.close();
-                    } catch (e) {
-                        console.log(e);
-                        console.log("phantomjs> Unable to save result of Suite '"+ suitesResults[i]["xmlfilename"] +"'");
+                if(typeof suitesResults.length !== 'undefined') {
+                    for ( var i = 0, len = suitesResults.length; i < len; ++i ) {
+                        try {
+                            f = fs.open(resultdir + '/' + suitesResults[i]["xmlfilename"], "w");
+                            f.write(suitesResults[i]["xmlbody"]);
+                            f.close();
+                        } catch (e) {
+                            console.log(e);
+                            console.log("phantomjs> Unable to save result of Suite '"+ suitesResults[i]["xmlfilename"] +"'");
+                        }
                     }
                 }
-                
+
                 // Return the correct exit status. '0' only if all the tests passed
                 phantom.exit(page.evaluate(function(){
                     return jasmine.phantomjsXMLReporterPassed ? 0 : 1; //< exit(0) is success, exit(1) is failure
