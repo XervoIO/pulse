bfea33170c6d546ab42248260c01d76f0384e015
Charlie Key <ckey@paranoidferret.com>
2012-02-03 12:53:05 -0500

    updates to drag drop code in core, a sprite that has had an item dropped on it will kick out 'item dropped' event now
    added new function from unbinding a specific callback

--- demos/tests/dragdrop.js
+++ demos/tests/dragdrop.js
@@ -1,19 +1,19 @@
 pulse.ready(function(){
   var engine = new pulse.Engine();
   var cybertron = new pulse.Scene({name: 'Cybertron'});
-  
+
   var world = new pulse.Layer({name: 'layer', x : 320, y : 240});
   world.zindex = 2;
-  world.events.bind('mousemove', function(evt) { 
+  world.events.bind('mousemove', function(evt) {
     var debugPos = document.getElementById('mousep');
     debugPos.innerText = evt.world.x + ', ' + evt.world.y;
   });
-  
+
   world.events.bind('keydown', function(evt) {
     var debugKey = document.getElementById('kdown');
     debugKey.innerText = evt.key + '[' + evt.keyCode + ']';
   });
-  
+
   var dropArea = new pulse.Sprite({src: '../img/grid.png'});
   dropArea.dropAcceptEnabled = true;
   dropArea.events.bind('dragenter', function(evt) {
@@ -28,7 +28,7 @@ pulse.ready(function(){
     var console = document.getElementById('console2');
     console.innerText = "Drag Exit - " + evt.world.x + " " + evt.world.y;
   });
-  
+
   var dragBox = new pulse.Sprite({src: '../img/blue_square.png'});
   dragBox.dragDropEnabled = true;
   dragBox.events.bind('dragstart', function(evt) {
@@ -39,16 +39,16 @@ pulse.ready(function(){
     var console = document.getElementById('console');
     console.innerText = "Drag Drop - " + evt.world.x + " " + evt.world.y;
   });
-  
+
   var dragAccept = new pulse.Sprite({src: '../img/green_square.png'});
   var dragRevoke = new pulse.Sprite({src: '../img/red_square.png'});
-  
+
   function loop(sceneManager)
-  { 
+  {
     var debugTime = document.getElementById('time');
     debugTime.innerText = engine.masterTime;
   }
-  
+
   function gameGo()
   {
     dragBox.position = {x: 35, y: 325};
@@ -56,17 +56,17 @@ pulse.ready(function(){
     dropArea.position = {x: 400, y: 150};
     dragAccept.visible = false;
     dragRevoke.visible = false;
-  
+
     world.addNode(dropArea);
     world.addNode(dragBox);
     world.addNode(dragAccept);
     world.addNode(dragRevoke);
-    
+
     cybertron.addLayer(world);
-    
+
     engine.scenes.addScene(cybertron);
     engine.scenes.activateScene(cybertron);
-    
+
     engine.go(20, loop);
   }
   gameGo();
