ded201bc40dce50ffa038327b2a9d97f685c6f54
Richard Key <rich@busyrich.com>
2012-02-19 16:17:09 -0700

    Updated the asset manager demo to reflect the changes made to managing assets.

--- demos/asset_manager/assets.js
+++ demos/asset_manager/assets.js
@@ -1,6 +1,5 @@
 pulse.ready(function() {
 
-  var myEngine = new pulse.Engine();
   var myManager = new pulse.AssetManager();
   var myBundle = new pulse.AssetBundle();
   var myBundleToo = new pulse.AssetBundle();
@@ -36,38 +35,20 @@ pulse.ready(function() {
   myManager.addBundle(myBundle, 'Bundle1');
   myManager.addBundle(myBundleToo, 'Bundle2');
   
-  function loop(sceneManager)
-  {
-    var rAsset = Math.floor(numAssets * Math.random());
-    var bundle = 'Bundle' + Math.floor(3 * Math.random());
-    var asset;
-    
-    switch(bundle)
-    {
-      case 'Bundle1':
-        rAsset = 'a1-' + rAsset;
-        asset = myManager.getAsset(rAsset, bundle);
-        break;
-      case 'Bundle2':
-        rAsset = 'a2-' + rAsset;
-        asset = myManager.getAsset(rAsset, bundle);
-        break;
-      default:
-        rAsset = 'global' + rAsset;
-        asset = myManager.getAsset(rAsset);
-        break;
-    }
-    
-    var debugTime = document.getElementById('loaded');
-    debugTime.innerHTML = '';
-    
-    debugTime.innerHTML = '<h1>' + myManager.percentLoaded + '%</h1>'
-      + '<h4>Random Asset:</h4>'
-      + asset.name
-      + '<br/>' + bundle
-      + '<br/>' + asset.filename; 
-  }
-
+  var loaded = document.getElementById('loaded');
+  var file = document.getElementById('file'); 
+  
+  myManager.events.bind('assetLoaded', function(evt) {
+    file.innerHTML = evt.asset + ' Loaded.';
+  });
+  
+  myManager.events.bind('progressChanged', function() {
+    loaded.innerHTML = '<h1>' + myManager.percentLoaded + '% Loaded</h1>';
+  });
+  
+  myManager.events.bind('complete', function(evt){
+    loaded.innerHTML = '<h1>All Done :D</h1>';
+  });
+  
   myManager.load();
-  myEngine.go(100, loop);
 });
