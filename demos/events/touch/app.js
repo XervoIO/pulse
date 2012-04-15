a0d8fb7598a938d1a2f5c0578d9828f9d577f398
Richard Key <rich@busyrich.com>
2012-04-14 18:02:56 -0700

    CANVASENGINE-118 - Added a touch and gesture events demo.

---
+++ demos/events/touch/app.js
@@ -0,0 +1,64 @@
+pulse.ready(function() {
+  var textures = [];
+  textures.push(new pulse.Texture({filename:'../../img/green_square.png'}));
+  textures.push(new pulse.Texture({filename:'../../img/blue_square.png'}));
+  textures.push(new pulse.Texture({filename:'../../img/red_square.png'}));
+
+  var world = new pulse.Layer({name: 'layer', x : 320, y : 240});
+  world.zindex = 2;
+  
+  world.events.bind('gestureend', function(e) {
+    var cns = document.getElementById('console');
+    cns.innerHTML += "Gesture End " + e.gestureRotation + ',' +  e.gestureScale +  "<br/>";
+  });
+  
+  var bg = new pulse.Layer({name: 'bg', x : 320, y : 240});
+  var bgs = new pulse.Sprite({
+    src: '../../img/gray_bg.jpg',
+    name: 'bg'
+  });
+  bgs.position = {x: 320, y: 240};
+  
+  bg.addNode(bgs);
+  bg.zindex = 1;
+
+  var cybertron = new pulse.Scene({name: 'cybertron'});
+
+  var engine = new pulse.Engine();
+
+  function loop(sceneManager)
+  {
+    var debugTime = document.getElementById('time');
+    debugTime.innerText = engine.masterTime;
+  }
+
+  function gameGo()
+  {
+    var s;
+    for(var i = 0; i < 9; i++) {
+      s = new pulse.Sprite({name: 'Box' + i, src: textures[i%3]});
+      s.anchor = {x: i % 3 * 0.5, y: Math.floor(i / 3) * 0.5};
+      s.position = {x: 106 + (i % 3 * 213), y: 35 + Math.floor(i / 3) * 120};
+      s.events.bind('click', function(e){
+        var cns = document.getElementById('console');
+        cns.innerText = e.sender.name + " box clicked " + Math.floor(Math.random() * 42);
+      });
+      s.events.bind('touchstart', function(e){
+        var cns = document.getElementById('console');
+        cns.innerHTML += e.sender.name + " clicked " + e.world.x + ',' +  e.world.y +  "<br/>";
+      });
+      world.addNode(s);
+    }
+
+    cybertron.addLayer(bg);
+
+    engine.scenes.addScene(cybertron);
+    engine.scenes.activateScene(cybertron);
+
+    cybertron.addLayer(world);
+
+    engine.go(16.67, loop);
+  }
+
+  gameGo();
+});
