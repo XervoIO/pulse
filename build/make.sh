b11b4bd9062617ba65921f558e5c3b0eba18142c
Brandon Cannaday <brandon@paranoidferret.com>
2012-06-10 15:24:47 -0400

    [CANVASENGINE-66] Initial implementation of the physics module.

--- build/make.sh
+++ build/make.sh
@@ -3,3 +3,6 @@ sh ../lib/pulse/build/make.pulse.sh
 
 # Make Debug Module
 sh ../lib/modules/pulse.debug/build/make.debug.sh
+
+# Make Physics Module
+sh ../lib/modules/pulse.physics/build/make.physics.sh
